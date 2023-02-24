8d9c007720bcac059829ca846cdf8ef1bd539baa
Ewintong <eddywintong@gmail.com>
2023-02-24 14:18:29 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,7 +9,7 @@
 int main(void)
 
 {
-	_putchar(' ');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -17,7 +17,8 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');
+	_putchar('\n')
 
 	return (0);
+
 }
